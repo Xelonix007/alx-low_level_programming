20cbd0803535d0ba377554cadf799e73658b8539
Super User <etochukwu739@gmail.com>
2023-09-07 07:55:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - a program that prints aline with a function.
+ * Return:0(success)
+ */
+int main(void){
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);	
+} 
